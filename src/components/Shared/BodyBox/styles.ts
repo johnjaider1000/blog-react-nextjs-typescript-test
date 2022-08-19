a16dff23340fb24baf1f7c2d39b2e2e10059df28
johnjaider1000 <jhonjaider100015@gmail.com>
2022-08-19 12:28:27 -0500

    Se mueven paquetes no necesarios

--- src/components/Shared/BodyBox/styles.ts
+++
@@ -1,9 +0,0 @@
-import { styled } from "@mui/material";
-
-interface Props {
-  padding?: string;
-}
-
-export default styled("div")<Props>`
-  padding: ${({ padding }) => (padding ? padding : "36px")};
-`;
